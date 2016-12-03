edfba2f09fb5a30b4ae30528a80c6004cb277211
Maciej Gawel <m.gawel@hotmail.com>
2016-12-03 22:01:38 +0100

    Add scopes to github login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, Rails.application.secrets.omniauth[:github_id], Rails.application.secrets.omniauth[:github_secret]
+  provider :github, Rails.application.secrets.omniauth[:github_id], Rails.application.secrets.omniauth[:github_secret],
+           scope: 'repo:status,write:repo_hook,read:org'
 end
