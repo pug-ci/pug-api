714c6727777f44af73992bd7e9e260a11c32ffdd
Maciej Gawel <m.gawel@hotmail.com>
2016-12-04 23:12:51 +0100

    Add date format

--- app/serializers/build_serializer.rb
+++ app/serializers/build_serializer.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 class BuildSerializer < ActiveModel::Serializer
   attributes :id, :status, :created_at
+
+  def created_at
+    I18n.l object.created_at, format: :short
+  end
 end
