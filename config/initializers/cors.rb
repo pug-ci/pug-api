3ca139d13b77c98b8472cd0219c751260afbc1e2
Maciej Gawel <m.gawel@hotmail.com>
2016-12-10 20:59:30 +0100

    Move cors origins to config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080'
+    origins Rails.application.secrets.cors_origins
 
     resource '*', headers: :any,
                   methods: [:get, :post, :put, :patch, :delete, :options, :head]
