0353ad6dd4a9a9384b26d0e7b864917c07905b5a
Maciej Gawel <m.gawel@hotmail.com>
2016-12-10 20:59:09 +0100

    Add build config to serializer

--- app/serializers/build_serializer.rb
+++ app/serializers/build_serializer.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 class BuildSerializer < ActiveModel::Serializer
-  attributes :id, :status, :created_at, :ref, :commit_id, :commit_url, :commit_message, :committer_username
+  attributes :id, :status, :created_at, :ref, :commit_id, :commit_url, :commit_message, :committer_username, :config
 
   def created_at
     I18n.l object.created_at, format: :short
+  end
+
+  def config
+    JSON.dump object.config
   end
 end
